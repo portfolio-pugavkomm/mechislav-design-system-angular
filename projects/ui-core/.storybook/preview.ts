e32157c9458b1607fa59e38f445a55c17276788c
Pugavkomm <slavapugavko2@gmail.com>
2025-10-13 23:35:04 -0300

    feat: add storybok

---
+++ projects/ui-core/.storybook/preview.ts
@@ -0,0 +1,18 @@
+import type {Preview} from '@storybook/angular'
+import { themes } from 'storybook/theming';
+
+const preview: Preview = {
+  parameters: {
+    docs: {
+      theme: themes.dark,
+    },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
---
+++ projects/ui-core/src/lib/button/button.stories.ts
@@ -0,0 +1,111 @@
+import type {Meta, StoryObj} from '@storybook/angular';
+import {Button} from './button';
+
+const _buttonTemplate = `<m-button [size]=size [variant]="variant" [disabled]="disabled">{{label ?? 'Click me'}}</m-button>`;
+
+const meta: Meta<Button> = {
+  title: 'UI KIT/Atoms/Button',
+  component: Button,
+  tags: ['autodocs'],
+
+  argTypes: {
+    variant: {
+      control: 'select',
+      options: ['primary', 'secondary', 'linked', 'canceled'],
+      description: 'Button variant',
+    },
+    // @ts-ignore
+    label: {
+      control: 'text',
+      description: 'The text displayed inside the button.',
+    },
+    size: {
+      control: 'select',
+      options: ['s', 'm', 'l'],
+      description: 'Button height',
+    },
+    disabled: {
+      control: 'boolean',
+      description: 'No active button'
+    },
+  },
+
+}
+
+export default meta;
+type Story = StoryObj<Button>;
+
+
+export const Playground: Story = {
+  render: (args) => ({
+    props: args,
+    template: _buttonTemplate,
+  }),
+  args: {
+    variant: 'primary',
+    size: 'm',
+    disabled: false,
+  },
+};
+
+export const Secondary: Story = {
+  render: (args) => ({
+    props: args,
+    template: _buttonTemplate,
+  }),
+  args: {
+    variant: 'secondary',
+    size: 'm',
+    disabled: false,
+  },
+};
+
+export const Linked: Story = {
+  render: (args) => ({
+    props: args,
+    template: _buttonTemplate,
+  }),
+  args: {
+    variant: 'linked',
+    size: 'm',
+    disabled: false,
+  },
+};
+
+
+export const Canceled: Story = {
+  render: (args) => ({
+    props: args,
+    template: _buttonTemplate,
+  }),
+  args: {
+    variant: 'canceled',
+    size: 'm',
+    disabled: false,
+  },
+};
+
+export const Medium: Story = {
+  render: (args) => ({
+    props: args,
+    template: _buttonTemplate,
+  }),
+  args: {
+    variant: 'primary',
+    size: 'm',
+    disabled: false,
+  }
+}
+
+
+export const Small: Story = {
+  render: (args) => ({
+    props: args,
+    template: _buttonTemplate,
+  }),
+  args: {
+    variant: 'primary',
+    size: 's',
+    disabled: false,
+  }
+};
