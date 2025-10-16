c397a3780cf4b6d54c88724aa786b16f18750f2c
Pugavkomm <slavapugavko2@gmail.com>
2025-10-16 08:44:40 -0300

    refactor: change button implementation with drective (#7)

--- projects/ui-core/src/lib/button/me-button.directive.ts
+++ projects/ui-core/src/lib/button/me-button.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, HostBinding, input} from '@angular/core';
+import {Directive, ElementRef, inject, input, signal} from '@angular/core';
 
 /**
  * One of button variants
@@ -12,7 +12,19 @@ export type ButtonSize = 's' | 'm' | 'l'; // small, medium (default), large
 
 @Directive({
   selector: 'button[meButton], a[meButton]',
-  standalone: true,
+  host: {
+    'class': 'me-button',
+    '[class.me-button--primary]': 'variant() === "primary"',
+    '[class.me-button--secondary]': 'variant() === "secondary"',
+    '[class.me-button--linked]': 'variant() === "linked"',
+    '[class.me-button--canceled]': 'variant() === "canceled"',
+    '[class.me-button--s]': 'size() === "s"',
+    '[class.me-button--m]': 'size() === "m"',
+    '[class.me-button--l]': 'size() === "l"',
+    '[attr.aria-disabled]': 'disabled()',
+    '[attr.disabled]': 'isButton() && disabled() ? "" : null',
+    '(click)': 'handleClick($event)'
+  }
 })
 export class MeButtonDirective {
   /**
@@ -30,18 +42,26 @@ export class MeButtonDirective {
    */
   size = input<ButtonSize>('m');
 
-  @HostBinding('class')
-  get hostClasses(): string {
-    return [
-      'me-button',
-      `me-button--${this.variant()}`,
-      `me-button--${this.size()}`,
-    ].join(' ');
+  protected readonly isButton = signal<boolean | undefined>(undefined);
+
+  constructor() {
+    const elem = inject(ElementRef).nativeElement as HTMLElement;
+    this.isButton.set(elem.tagName === 'BUTTON');
   }
 
-  @HostBinding('attr.aria-disabled')
-  @HostBinding('disabled')
-  get isDisabled(): boolean {
-    return this.disabled();
+  /**
+   * Prevent the default action while disabled
+   *
+   * It can be used for <button or <a tags
+   *
+   * @param event
+   */
+  handleClick(event: Event): void {
+    if (this.disabled()) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
   }
+
+
 }
