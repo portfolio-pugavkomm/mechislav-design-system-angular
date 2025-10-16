8cfcf0cfb3e7566d15a8bd330688f42aebb117b6
Pugavkomm <slavapugavko2@gmail.com>
2025-10-16 09:24:27 -0300

    Test/configure tests (#8)
    * fix: correct default tests and remove storybook
    
    * test: add new karma config and pre commit test hook

--- projects/ui-core/src/lib/button/me-button.directive.spec.ts
+++ projects/ui-core/src/lib/button/me-button.directive.spec.ts
@@ -1,8 +1,34 @@
+import {Component, provideZonelessChangeDetection} from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { MeButtonDirective } from './me-button.directive';
 
-describe('MeButton', () => {
+@Component({
+  standalone: true,
+  imports: [MeButtonDirective],
+  template: `<button meButton>Test Button</button>`
+})
+class TestHostComponent {}
+
+describe('MeButtonDirective', () => {
+  let fixture: ComponentFixture<TestHostComponent>;
+  let directive: MeButtonDirective;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TestHostComponent],
+      providers: [provideZonelessChangeDetection()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TestHostComponent);
+
+    const directiveEl = fixture.debugElement.query(By.directive(MeButtonDirective));
+    directive = directiveEl.injector.get(MeButtonDirective);
+
+    fixture.detectChanges();
+  });
+
   it('should create an instance', () => {
-    const directive = new MeButtonDirective();
     expect(directive).toBeTruthy();
   });
 });
--- projects/ui-core/src/lib/tag/m-tag.component.spec.ts
+++ projects/ui-core/src/lib/tag/m-tag.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MTag } from './m-tag.component';
+import {provideZonelessChangeDetection} from '@angular/core';
 
 describe('Tag', () => {
   let component: MTag;
@@ -8,7 +9,8 @@ describe('Tag', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MTag]
+      imports: [MTag],
+      providers: [provideZonelessChangeDetection()]
     })
     .compileComponents();
 
